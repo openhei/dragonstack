afb7fcba729cd554f23022f9ca204b8d8cb35723
openhei <qweb777@gmail.com>
2018-10-05 18:37:27 -0400

    initial

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const Dragon = require("./dragon");
+
+const fooey = new Dragon({ birthdate: new Date(), nickname: "fooey" });
+
+const baloo = new Dragon({ birthdate: new Date(), nickname: "baloo" });
+
+const mimar = new Dragon();
+
+setTimeout(() => {
+  const gooby = new Dragon();
+  console.log("gooby", gooby);
+}, 3000);
+
+console.log("fooey", fooey);
+console.log("baloo", baloo);
+console.log("mimar", mimar);
